3d8f983f1791081e549fe95068ad4e5f943c7174
Jules2791 <may.julie@yahoo.co.uk>
2025-01-09 08:05:50 +0000

    Adding Javascript date and time

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,1 +1,26 @@
+function updateTime() {
+  // Cape Town
+  let capeTownElement = document.querySelector("#cape-town");
+  let capeTownDateElement = capeTownElement.querySelector(".date");
+  let capeTownTimeElement = capeTownElement.querySelector(".time");
+  let capeTownTime = moment().tz("Africa/Johannesburg");
 
+  capeTownDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  capeTownTimeElement.innerHTML = capeTownTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  // Sydney
+  let sydenyElement = document.querySelector("#sydney");
+  let sydenyDateElement = sydenyElement.querySelector(".date");
+  let sydenyTimeElement = sydenyElement.querySelector(".time");
+  let sydenyTime = moment().tz("Australia/Sydney");
+
+  sydenyDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  sydenyTimeElement.innerHTML = sydenyTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1000);
